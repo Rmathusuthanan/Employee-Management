3e7a74c2ce5e56eef7b359287d9d7fb73d14e66e
Rmathusuthanan <rmathusuthanan@gmail.com>
2026-01-15 21:36:00 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome" });
 });
 
+//Get Employees Details
+app.get("/employee", (req, res) => {});
+//POST Employees Details
+app.post("/employee", (req, res) => {});
+//Update Employees Details
+app.put("/employee/:id", (req, res) => {});
+//Delete Employees Details
+app.delete("/employee/:id", (req, res) => {});
+
 app.listen(PORT, () => {
   console.log(`Server Running on Port no ${PORT}`);
 });
