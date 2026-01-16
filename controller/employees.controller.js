cd194b1fd0dd95ddf4b7d2191ecd55088ff164d2
Rmathusuthanan <rmathusuthanan@gmail.com>
2026-01-16 11:27:33 +0530

    connected mongoDB

--- controller/employees.controller.js
+++ controller/employees.controller.js
@@ -1,11 +1,25 @@
+const Employee = require("../models/Employee.model");
+
 const Employees = (req, res) => {
   console.log(req.body);
   res.send(req.body);
 };
 
-const addEmployees = (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+const addEmployees = async (req, res) => {
+  const createEmployee = new Employee({
+    name: req.body.name,
+    age: req.body.age,
+    email: req.body.email,
+    gender: req.body.gender,
+  });
+
+  console.log("Schema", createEmployee);
+  try {
+    const employee = await createEmployee.save();
+    return res.status(201).json(employee);
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
 };
 const UpdateEmployees = (req, res) => {
   res.json({ mgs: "Update Employees" });
---
+++ models/Employee.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const employeeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Employee = model("Employee", employeeSchema);
+
+module.exports = Employee;
